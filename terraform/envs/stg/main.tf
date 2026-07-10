900a9706c2f8c07dfec9a3a9d869bc3a1995393c
vinh25042005 <vinh25042005@gmail.com>
2026-07-10 09:17:20 +0000

    fix: sửa helm provider syntax (kubernetes=), chart_path, set=[...] cho helm_release

--- terraform/envs/stg/main.tf
+++ terraform/envs/stg/main.tf
@@ -43,7 +43,7 @@ provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     config_path = var.kubeconfig_path
   }
 }
@@ -78,7 +78,7 @@ module "k8s_app" {
   frontend_image = var.frontend_image
   replicas       = var.replicas
   ingress_host   = var.ingress_host
-  chart_path     = abspath("${path.module}/../../helm/techshop")
+  chart_path     = abspath("${path.module}/../../../helm/techshop")
   depends_on     = [terraform_data.fetch_kubeconfig]
 }
 
