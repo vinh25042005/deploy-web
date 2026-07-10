a1d9749017e46797b3ba74bdaf75f69866fd8131
vinh25042005 <vinh25042005@gmail.com>
2026-07-10 08:34:09 +0000

    feat(terraform): envs/dev - root config dùng module network, compute, loadbalancer, rancher, k8s-app

---
+++ terraform/envs/dev/variables.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+  type    = string
+  default = "techshop"
+}
+variable "region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "key_name" {
+  type    = string
+  default = "techshop-key"
+}
+
+# ── K8s App variables ──
+variable "kubeconfig_path" {
+  type    = string
+  default = "~/.kube/techshop-config"
+}
+
+variable "backend_image" {
+  type    = string
+  default = "asia-southeast1-docker.pkg.dev/techshop-prod-2026/techshop/backend:latest"
+}
+
+variable "frontend_image" {
+  type    = string
+  default = "asia-southeast1-docker.pkg.dev/techshop-prod-2026/techshop/frontend:latest"
+}
+
+variable "replicas" {
+  type    = number
+  default = 2
+}
+
+variable "ingress_host" {
+  type    = string
+  default = "dev.techshop.local"
+}
